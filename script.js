bd4df8d7b104854052e622a70a6bc0948ffe1fbc
Markham Shofner <markham.shofner@gmail.com>
2015-10-26 11:09:16 -0400

    restructure script as an object

--- script.js
+++ script.js
@@ -1,55 +1,59 @@
 // wait til ready
 $(document).ready(function() {
+
+  // put all functions inside an object
+  var warGame = {
+
   // TEST Code
-  $("p").on("click", function() {
-    $(this).css("background", "yellow");
-  });
+  // $("p").on("click", function() {
+  //   $(this).css("background", "yellow");
+  // });
 
   // initialize the decks
-  var deckMain = [];
-  var deck1 = [];
-  var deck2 = [];
+  deckMain: [],
+  deck1: [],
+  deck2: [],
 
-  var suits = ["hearts", "diamonds", "spades", "clubs"];
+  suits: ["hearts", "diamonds", "spades", "clubs"],
 
-  function makeDeck() {
-    for (var i = 0; i < suits.length; i++) {
+  makeDeck: function () {
+    for (var i = 0; i < this.suits.length; i++) {
       for (var j = 2; j <= 14; j++) {
         var card = {
           rank: j,
-          suit: suits[i],
+          suit: this.suits[i],
         };
-        deckMain.push(card);
+        this.deckMain.push(card);
       }
     }
-  }
+  },
 
-  function shuffleSplit() {
+  shuffleSplit: function () {
     // split random and equal set of 26 cards into each player's deck
-
     // TODO clean mechanism to toggle between which array to deal towards
     var deal = 1;
-    while (deckMain.length > 0) {
-      var randomIndex = Math.floor(Math.random() * deckMain.length);
-      var cardMove = deckMain.splice(randomIndex, 1);
+    while (this.deckMain.length > 0) {
+      var randomIndex = Math.floor(Math.random() * this.deckMain.length);
+      var cardMove = this.deckMain.splice(randomIndex, 1);
       if (deal === 1) {
-        deck1.push(cardMove);
+        this.deck1.push(cardMove);
         deal = 2;
       } else {
-        deck2.push(cardMove);
+        this.deck2.push(cardMove);
         deal = 1;
       }
     }
     // TEST code to see player decks
-    console.log(deck1);
-    console.log(deck2);
-  }
+    console.log(this.deck1);
+    console.log(this.deck2);
+  },
 
-  function initializeGame() {
-    makeDeck();
-    shuffleSplit();
+  initializeGame: function () {
+    this.makeDeck();
+    this.shuffleSplit();
   }
+};
 
-  initializeGame();
+  warGame.initializeGame();
 
 });
