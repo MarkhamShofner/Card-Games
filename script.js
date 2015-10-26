1b10f063d2c60d01f1d8d5ef35c829a02c2db24d
Markham Shofner <markham.shofner@gmail.com>
2015-10-26 11:42:09 -0400

    add draft functions to pull cards and determine the winner of the battle

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
   deckMain: [],
   deck1: [],
   deck2: [],
-
   suits: ["hearts", "diamonds", "spades", "clubs"],
 
   makeDeck: function () {
@@ -44,8 +43,38 @@ $(document).ready(function() {
       }
     }
     // TEST code to see player decks
-    console.log(this.deck1);
-    console.log(this.deck2);
+    // console.log(this.deck1);
+    // console.log(this.deck2);
+  },
+
+  // compare value of two cards and assign an outcome and the battle-array
+  compareCards: function (card1, card2) {
+    var outcome = {
+      result: "",
+      array: [],
+    };
+    // console.log(card1.rank);
+    // console.log(card2.rank);
+    if (card1.rank > card2.rank) {
+      outcome.result = "player1";
+      outcome.array.push(card1,card2);
+    } else if (card2.rank > card1.rank) {
+      outcome.result = "player2";
+      outcome.array.push(card2,card1);
+    } else {
+      outcome.result = "tie";
+      outcome.array.push(card1,card2);
+    }
+    // console.log(outcome);
+  },
+
+
+  determineWinner: function () {
+    var card1 = this.deck1[0];
+    var card2 = this.deck2[0];
+    var outcome = (this.compareCards(card1,card2));
+    console.log("5");
+    console.log(outcome);
   },
 
   initializeGame: function () {
@@ -54,6 +83,15 @@ $(document).ready(function() {
   }
 };
 
+  var test1 = {
+    rank: 14
+  };
+  var test2 = {
+    rank: 14
+  };
+
+  warGame.compareCards (test1, test2);
   warGame.initializeGame();
+  warGame.determineWinner();
 
 });
