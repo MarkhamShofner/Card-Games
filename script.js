535f5d309189482ce57c64fdbbd1822e40731169
Markham Shofner <markham.shofner@gmail.com>
2015-10-26 13:34:30 -0400

    version before adding interactive / clickable functionality

--- script.js
+++ script.js
@@ -4,17 +4,13 @@ $(document).ready(function() {
   // put all functions inside an object
   var warGame = {
 
-    // TEST Code
-    // $("p").on("click", function() {
-    //   $(this).css("background", "yellow");
-    // });
 
-    // initialize the decks
     deckMain: [],
     deck1: [],
     deck2: [],
     suits: ["hearts", "diamonds", "spades", "clubs"],
 
+    // initialize the decks
     makeDeck: function() {
       for (var i = 0; i < this.suits.length; i++) {
         for (var j = 2; j <= 14; j++) {
@@ -25,6 +21,7 @@ $(document).ready(function() {
           this.deckMain.push(card);
         }
       }
+      console.log(this.deckMain);
     },
 
     shuffleSplit: function() {
@@ -35,10 +32,10 @@ $(document).ready(function() {
         var randomIndex = Math.floor(Math.random() * this.deckMain.length);
         var cardMove = this.deckMain.splice(randomIndex, 1);
         if (deal === 1) {
-          this.deck1.push(cardMove);
+          this.deck1.push(cardMove[0]);
           deal = 2;
         } else {
-          this.deck2.push(cardMove);
+          this.deck2.push(cardMove[0]);
           deal = 1;
         }
       }
@@ -60,25 +57,37 @@ $(document).ready(function() {
         outcome.result = "tie";
         outcome.array.push(card1, card2);
       }
+      console.log(card1.rank);
+      console.log(card2.rank);
+      console.log(outcome);
       return outcome;
     },
 
     playHand: function() {
       var cardA = this.deck1[0];
       var cardB = this.deck2[0];
-      var outcome = (this.compareCards(cardA[0], cardB[0]));
+      var outcome = (this.compareCards(cardA, cardB));
       if (outcome.result === "player1") {
         console.log("player 1 wins the battle!");
-        this.deck1.push(outcome.array);
+        for (var i = 0; i < outcome.array.length; i++) {
+          this.deck1.push(outcome.array[i]);
+        }
       } else if (outcome.result === "player2") {
-        console.log("player 1 wins the battle!");
-        this.deck2.push(outcome.array);
+        console.log("player 2 wins the battle!");
+        for (var j = 0; j < outcome.array.length; j++) {
+          this.deck2.push(outcome.array[j]);
+        }
       } else {
-        log("WAR!!!");
+        //TODO figure out where to determine war
+        console.log("WAR!!!");
       }
       console.log(this.deck1);
       console.log(this.deck2);
     },
+
+    // callHand: $("p").on("click", function() {
+    //   $(this).css("background", "yellow");
+    // }),
 
     initializeGame: function() {
       this.makeDeck();
